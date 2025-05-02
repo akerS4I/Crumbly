0f3035ae2a094be48543d68da6f8d923f02023c1
akerS4I <birdblack283474@gmail.com>
2025-05-02 10:50:22 +0500

    Project env ready

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+
 # Crumbly Project
+
+Project in development stage
+
+Team:
+
+- [@akerS4I](https://github.com/akerS4I)
+- Michael
+- Ruby
+## Tech Stack
+
+**React, Typescript + SWC, SCSS, Vite, Github pages.**
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/index.scss'
+
+// Pages import
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+    {
+      path: '/contact',
+      element: <Contact />,
+    },
+  ]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
 )
