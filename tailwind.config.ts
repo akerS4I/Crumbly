4e85b6ce30b376ee2804f4e87c16589f22b398cc
akerS4I <birdblack283474@gmail.com>
2025-05-07 01:41:09 +0500

    Switched to tailwind + shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import tailwindAnimate from "tailwindcss-animate";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
