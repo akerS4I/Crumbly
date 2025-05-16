1ad4cbe3d55078b3fd4cb27282b8e586a61e88f0
akerS4I <birdblack283474@gmail.com>
2025-05-16 23:17:33 +0500

    Video moved from file directory to youtube

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 
 const VideoPage = () => {
@@ -7,11 +8,16 @@ const VideoPage = () => {
       <h1 className="text-center text-xl font-medium mb-8 mt-4">
         Окунитесь в разработку нашего сайта с головой просмотрев короткое видео
       </h1>
-      <video
-        src="/videos/VideoPresentation.mp4"
-        controls
-        className="w-[95%] max-w-3xl mx-auto rounded-lg"
-      />
+      <div className="w-[95%] max-w-3xl aspect-video mx-auto rounded-lg overflow-hidden mb-30">
+        <iframe
+          className="w-full h-full"
+          src="https://www.youtube.com/embed/CPk5e-b0r6Q"
+          title="Video presentation"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+      <Footer/>
     </>
   );
 };
