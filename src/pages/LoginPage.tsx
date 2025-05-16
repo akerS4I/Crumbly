72d7cc723eb68464d4710176e4fcb4b184811b3f
akerS4I <birdblack283474@gmail.com>
2025-05-16 23:45:05 +0500

    Return buttons on login pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,14 @@
 import { LoginForm } from "@/components/login-form";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <Link to="/">
+          <Button className="mb-6 w-full">Вернуться на главную</Button>
+        </Link>
         <LoginForm />
       </div>
     </div>
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,9 +1,14 @@
 import { SignupForm } from "@/components/signup-form";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const SignupPage = () => {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <Link to="/">
+          <Button className="mb-6 w-full">Вернуться на главную</Button>
+        </Link>
         <SignupForm />
       </div>
     </div>
